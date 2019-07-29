1a375a40546be5b07154d7e42f764c6c91988497
vershinin_so <vershinin_so@bel-rusnarbank.ru>
2019-07-29 17:39:36 +0300

    build debian buster

--- php-fpm-gost/make_dockerfile.sh
+++ php-fpm-gost/make_dockerfile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DOCKER_BASE_URL="https://raw.githubusercontent.com/docker-library/php/master/7.3/stretch/fpm"
-ORIGINAL_INSTRUCTION="FROM debian:stretch-slim"
+DOCKER_BASE_URL="https://raw.githubusercontent.com/docker-library/php/master/7.3/buster/fpm"
+ORIGINAL_INSTRUCTION="FROM debian:buster-slim"
 
 DOCKER_BUILD_FILES=( Dockerfile docker-php-entrypoint docker-php-ext-configure docker-php-ext-enable docker-php-ext-install docker-php-source)
 for FILE in "${DOCKER_BUILD_FILES[@]}"
@@ -10,7 +10,7 @@ do
 done
 
 STAGE_INSTRUCTIONS=$(cat << END
-FROM rnix/openssl-gost AS openssl-gost
+FROM gost-debian AS openssl-gost
 
 ${ORIGINAL_INSTRUCTION}
 
